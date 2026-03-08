0cec1a6b8fb79be487623df9b7d0418620076c90
AnhNguyenCong <nashtech_anh.nguyencong@privalgo.co.uk>
2026-03-08 16:29:19 +0700

    Init project

---
+++ Algorithms/Greedy/JobSequencingWithDeadlines/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace JobSequencingWithDeadlines
+{
+    public class Job
+    {
+        public string Id { get; set; }
+        public int Profit { get; set; }
+        public int Deadline { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Job[] jobs = new Job[]
+            {
+                new Job{ Id ="J1", Profit=5,  Deadline=3 },
+                new Job{ Id ="J2", Profit=10, Deadline=1 },
+                new Job{ Id ="J3", Profit=20, Deadline=2 },
+                new Job{ Id ="J4", Profit=15, Deadline=2 },
+                new Job{ Id ="J5", Profit=1,  Deadline=3 },
+            };
+
+            var sortedJobs = jobs.OrderByDescending((j) => j.Profit).ToArray();
+
+            PrintJobs(sortedJobs);
+
+            int maxDeadline = sortedJobs.Max(j => j.Deadline);
+            string[] result = new string[maxDeadline + 1];
+            int totalProfit = 0; //solution
+
+            foreach (var job in sortedJobs)
+            {
+                for (int slot = job.Deadline; slot >= 1; slot--)
+                {
+                    if (result[slot] == null)
+                    {
+                        result[slot] = job.Id;
+                        totalProfit += job.Profit;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Solution: {totalProfit}");
+        }
+
+        public static void PrintJobs(Job[] jobs)
+        {
+            foreach (var job in jobs)
+            {
+                Console.WriteLine($"{job.Id} - Profit: {job.Profit} - Deadline: {job.Deadline}");
+            }
+        }
+    }
+}
---
+++ Algorithms/Greedy/Knapsack/KnapsackAlgorithm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+///Implement Fractional Knapsack Algorithm in C# (O(nlogn))
+namespace Knapsack
+{
+    public class Item
+    {
+        public int Value { get; set; }
+        public int Weight { get; set; }
+        public double Ratio => (double)Value / Weight;
+    }
+    public class KnapsackAlgorithm
+    {
+        public static double GetMaxValue(int capacity, List<Item> items)
+        {
+            items.Sort((a, b) => b.Ratio.CompareTo(a.Ratio));
+
+            double totalValue = 0.0;
+            int currentWeight = 0;
+
+            foreach (var item in items)
+            {
+                if (currentWeight + item.Weight <= capacity)
+                {
+                    currentWeight += item.Weight;
+                    totalValue += item.Value;
+                }
+                else
+                {
+                    int remaining = capacity - currentWeight;
+                    totalValue += item.Ratio * remaining;
+                    break;
+                }
+            }
+
+            return totalValue;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>
+            {
+                new Item {Value = 60, Weight = 10},
+                new Item {Value = 100, Weight = 20},
+                new Item {Value = 120, Weight = 30}
+            };
+           
+            int capacity = 50;
+
+            double result = GetMaxValue(capacity, items);
+            Console.WriteLine("Maximum value in Knapsack = " + result);
+        }
+    }
+}
